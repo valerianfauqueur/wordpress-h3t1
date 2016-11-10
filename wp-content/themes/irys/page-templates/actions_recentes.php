7d142c060da9e59c56c708ce6f3d2e682a5060aa
armandfardeau <fardeauarmand@gmail.com>
2016-11-10 16:04:55 +0100

    create page templates

---
+++ wp-content/themes/irys/page-templates/actions_recentes.php
@@ -0,0 +1,9 @@
+/*
+Template Name: actions récentes
+*/
+
+<?php get_header(); //appel du template header.php  ?>
+
+
+
+<?php get_footer(); //appel du template header.php  ?>
---
+++ wp-content/themes/irys/page-templates/bio.php
@@ -0,0 +1,9 @@
+/*
+Template Name: biographie
+*/
+
+<?php get_header(); //appel du template header.php  ?>
+
+
+
+<?php get_footer(); //appel du template header.php  ?>
