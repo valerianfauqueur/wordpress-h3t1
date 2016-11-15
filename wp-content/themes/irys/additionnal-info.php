a0f75bc9f32157a93e271496fb674c18cc432f35
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-11-15 20:16:05 +0100

    [template:partial] Create template the bottom's CTA

---
+++ wp-content/themes/irys/additionnal-info.php
@@ -0,0 +1,18 @@
+<section class=''>
+    <section class=''>
+        <h3 class=''>Irys a besoin de votre soutient</h3>
+        <p class=''>
+            Chaque mois, je rencontre les salariés de différentes entreprises afin de les accompagner et comprendre les problèmes auxquels ils sont confrontés.
+        </p>
+    </section>
+    <section class=''>
+        <h3 class=''>Comment fonctionne Irys</h3>
+        <ul class=''>
+            <li>- Différentes entreprises afin de les accompagner.</li>
+            <li>- Comprendre les problèmes auxquels ils sont.</li>
+            <li>- Chaque mois, je rencontre les salariés.</li>
+            <li>- Différentes entreprises afin de les accompagner.</li>
+            <li>- Comprendre les problèmes auxquels ils sont.</li>
+        </ul>
+    </section>
+</section>
