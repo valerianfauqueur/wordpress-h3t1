5f578a2c3ae087753af8478413ee83aaadeaeb96
Valérian Fauqueur <valerian.fauqueur@hetic.net>
2016-11-15 12:32:47 +0100

    [bio:php] Add custom post type for bio

--- wp-content/themes/irys/inc/custom_post_types.php
+++ wp-content/themes/irys/inc/custom_post_types.php
@@ -3,7 +3,7 @@
 // ------------------ ANNONCES ----------------------
 add_action('init', 'programme_post_type');
 function programme_post_type(){
-    
+
     $labels = array(
         'name'               => 'Programme',
         'singular_name'      => 'Chapitre',
@@ -37,6 +37,42 @@ function programme_post_type(){
         'rewrite'             => array( 'slug' => 'programme' )
     );
     register_post_type('programme', $args );
-
 }
 
+// ------------------ BIO ----------------------
+add_action('init', 'bio_post_type');
+function bio_post_type(){
+    $labels = array(
+        'name'               => 'Biographie',
+        'singular_name'      => 'Bio part',
+        'all_items'          => 'Toutes les parties',
+        'add_new'            => 'Ajouter une partie',
+        'add_new_item'       => 'Ajouter une partie',
+        'edit_item'          => "Modifier la partie",
+        'new_item'           => 'Nouvelle partie',
+        'view_item'          => "Voir la partie",
+        'search_items'       => 'Trouver une partie',
+        'not_found'          => 'Pas de résultat',
+        'not_found_in_trash' => 'Pas de résultat',
+        'parent_item_colon'  => 'Parties parents:',
+        'menu_name'          => 'Biographie',
+    );
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','editor', 'excerpt'),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 2,
+        'menu_icon'           => 'dashicons-id-alt',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => 'programme' )
+    );
+    register_post_type('biographie', $args);
+}
