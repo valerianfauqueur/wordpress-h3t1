9f190693e85f546eeb12a4778d81eb3e0ce24ac8
armandfardeau <fardeauarmand@gmail.com>
2016-11-15 04:29:33 +0100

    Add footer menu

--- wp-content/themes/irys/footer.php
+++ wp-content/themes/irys/footer.php
@@ -1,10 +1,17 @@
-        <footer id="footer">
-        <!--
-            Tout le contenu de la partie head de mon site
-         -->
-        </footer>
+<footer id="footer" class="footer">
+    <?php
+    $args = array(
+        'theme_location' => 'footer', // nom du slug
+        'menu' => 'footer_fr', // nom à donner cette occurence du menu
+        'menu_class' => 'menu_footer container', // class à attribuer au menu
+        'menu_id' => 'fooyer_id' // id à attribuer au menu
+    );
+    wp_nav_menu($args);
+    ?>
+    <div class="footer_copyright">Irys programms - All Rights Reserved</div>
+</footer>
 
-        <!-- Execution de la fonction wp_footer() obligatoire ! -->
-        <?php wp_footer();  ?>
-    </body>
+<!-- Execution de la fonction wp_footer() obligatoire ! -->
+<?php wp_footer(); ?>
+</body>
 </html>
