73a3336514f65ae08372c1e4e584c664fee70b88
armandfardeau <fardeauarmand@gmail.com>
2016-11-15 15:51:56 +0100

    add class and html structure

--- wp-content/themes/irys/footer.php
+++ wp-content/themes/irys/footer.php
@@ -1,14 +1,28 @@
-<footer id="footer" class="footer">
-    <?php
-    $args = array(
-        'theme_location' => 'footer', // nom du slug
-        'menu' => 'footer_fr', // nom à donner cette occurence du menu
-        'menu_class' => 'menu_footer container', // class à attribuer au menu
-        'menu_id' => 'fooyer_id' // id à attribuer au menu
-    );
-    wp_nav_menu($args);
-    ?>
-    <div class="footer_copyright">Irys programms - All Rights Reserved</div>
+<footer>
+    <div class="container">
+        <nav>
+            <?php
+            $args = array(
+                'theme_location' => 'footer', // nom du slug
+                'menu' => 'footer_fr', // nom à donner cette occurence du menu
+                'menu_class' => 'menu_footer container', // class à attribuer au menu
+                'menu_id' => 'fooyer_id' // id à attribuer au menu
+            );
+            wp_nav_menu($args);
+            ?>
+        </nav>
+        <div class="social">
+            <div class="social__facebook">
+                <p><a>Lien Facebook</a></p>
+            </div>
+            <div class="social__twitter">
+                <p><a>Lien Twitter</a></p>
+            </div>
+        </div>
+        <div class="legal">
+            <p>Irys programms - All Rights Reserved</p>
+        </div>
+    </div>
 </footer>
 
 <!-- Execution de la fonction wp_footer() obligatoire ! -->
