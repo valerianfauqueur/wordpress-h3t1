87830a8ff368b8ab2e6ea63f7204661592870acc
armandfardeau <fardeauarmand@gmail.com>
2016-11-15 20:04:34 +0100

    add dynamic content and correct css class

--- wp-content/themes/irys/inc/custom_post_types.php
+++ wp-content/themes/irys/inc/custom_post_types.php
@@ -40,3 +40,81 @@ function programme_post_type(){
 
 }
 
+// ------------------ Actions à venir ----------------------
+add_action('init', 'actions_a_venir');
+function actions_a_venir(){
+
+    $labels = array(
+        'name'               => 'actions',
+        'singular_name'      => 'action',
+        'all_items'          => 'Toutes les actions',
+        'add_new'            => 'Ajouter une action',
+        'add_new_item'       => 'Ajouter une action',
+        'edit_item'          => "Modifier l'action",
+        'new_item'           => 'Nouvelle action',
+        'view_item'          => "Voir les actions",
+        'search_items'       => 'Trouver une action',
+        'not_found'          => 'Pas d\'action',
+        'not_found_in_trash' => 'Pas d\'action',
+        'parent_item_colon'  => 'action parentes:',
+        'menu_name'          => 'actions',
+    );
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','editor', 'excerpt' ),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 4,
+        'menu_icon'           => 'dashicons-megaphone',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => 'actions' )
+    );
+    register_post_type('actions', $args );
+
+}
+// ------------------ agenda ----------------------
+add_action('init', 'agenda');
+function agenda(){
+
+    $labels = array(
+        'name'               => 'date',
+        'singular_name'      => 'date',
+        'all_items'          => 'Toutes les dates',
+        'add_new'            => 'Ajouter une date',
+        'add_new_item'       => 'Ajouter une date',
+        'edit_item'          => "Modifier la date",
+        'new_item'           => 'Nouvelle date',
+        'view_item'          => "Voir les dates",
+        'search_items'       => 'Trouver une date',
+        'not_found'          => 'Pas de date',
+        'not_found_in_trash' => 'Pas de date',
+        'parent_item_colon'  => 'dates parentes:',
+        'menu_name'          => 'agenda',
+    );
+    $args = array(
+        'labels'              => $labels,
+        'hierarchical'        => false,
+        'supports'            => array( 'title','editor', 'excerpt' ),
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-megaphone',
+        'show_in_nav_menus'   => true,
+        'publicly_queryable'  => true,
+        'exclude_from_search' => false,
+        'has_archive'         => false,
+        'query_var'           => true,
+        'can_export'          => true,
+        'rewrite'             => array( 'slug' => 'date' )
+    );
+    register_post_type('agenda', $args );
+
+}
--- wp-content/themes/irys/page-templates/actions_recentes.php
+++ wp-content/themes/irys/page-templates/actions_recentes.php
@@ -5,84 +5,92 @@
 ?>
 <?php get_header(); //appel du template header.php  ?>
 <!--Hero section-->
-<section class="actions_recentes__hero">
-    <?php if (get_field('hero_img')): ?>
-        <img class="actions_recentes__hero-img" src="<?php the_field('hero_img'); ?>"/>
-    <?php endif; ?>
-
-    <p class="actions_recentes__hero-baseline"><?php the_field('hero_baseline'); ?></p>
-    <a class="actions_recentes__hero-button" type="button"
-       href="<?php the_field('hero_button_link'); ?>"><?php the_field('button_hero_text'); ?></a>
-    <h2 class="actions_recentes__hero-description-title"><?php the_field('hero_description_titre'); ?></h2>
-    <p class="actions_recentes__hero-description-text"><?php the_field('hero_description'); ?></p>
-
-</section>
-<section class="actions_recentes__evenement">
-    <div class="actions_recentes__evenement-1">
-        <?php if (get_field('evenement_image')): ?>
-            <img class="actions_recentes__evenement-img" src="<?php the_field('evenement_image'); ?>"/>
-        <?php endif; ?>
-
-        <h3 class="actions_recentes__evenement-title"><?php the_field('evenement_titre'); ?></h3>
-        <p class="actions_recentes__evenement-texte"><?php the_field('evenement_texte'); ?></p>
-
-
+<header class="actions">
+    <div class="header__background"></div>
+    <div class="container">
+        <div class="header__titles">
+            <h1>    <?php the_field('hero_baseline'); ?></h1>
+            <h2><?php the_field('hero_baseline2'); ?></h2>
+            <div class="btn"><a
+                    href="<?php the_field('hero_button_link'); ?>"><?php the_field('button_hero_text'); ?></a></div>
+        </div>
     </div>
-    <div class="actions_recentes__evenement-2">
-        <?php if (get_field('evenement_image_2')): ?>
-            <img class="actions_recentes__evenement-img" src="<?php the_field('evenement_image_2'); ?>"/>
-        <?php endif; ?>
+</header>
+<main>
+    <div class="container">
+        <section class="region">
+            <h3 class="region__title"><?php the_field('hero_description_titre'); ?></h3>
+            <p class="region__txt"><?php the_field('hero_description'); ?></p>
+        </section>
+    </div>
+    <div class="events">
+        <?php
+        // The Query
+        $args = array('post_type' => "actions");
+        $the_query = new WP_Query($args);
+        // The Loop
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                ?>
+                <article class="event">
+                    <div class="event__infos">
+                        <h4 class="event__title"><?php the_title() ?></h4>
+                        <p class="event__txt"><?php the_field('action_texte') ?></p>
+                        <p class="social">
+                            <a> <?php the_field('twitter_link') ?><img src="<?php the_field('twitter_icon') ?>" </a>
+                            <a> <?php the_field('facebook_link') ?><img src="<?php the_field('facebook_icon') ?>" </a>
+                        </p>
+                        <div class="event__img"><img src="<?php the_field('action_img') ?>" alt="<?php the_title() ?>"/>
+                        </div>
+                    </div>
+                </article>
+                <?php
+            }
+            /* Restore original Post Data */
+            wp_reset_postdata();
+        } else {
+            // no posts found
+        }
+        ?>
+    </div>
+    <article class="nextEvents">
+        <?php
+        // The Query
+        $args = array('post_type' => "agenda");
+        $the_query = new WP_Query($args);
+        // The Loop
+        if ($the_query->have_posts()) {
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                ?>
+                <article class="nextEvent">
+                    <p class="nextEvent__date"><?php the_title() ?></p>
+                    <p class="nextEvent__txt"><?php the_field('agenda_texte') ?></p>
+                </article>
+                <?php
+            }
+            /* Restore original Post Data */
+            wp_reset_postdata();
+        } else {
+            // no posts found
+        }
+        ?>
 
-        <h3 class="actions_recentes__evenement-title"><?php the_field('evenement_titre_2'); ?></h3>
-        <p class="actions_recentes__evenement-texte"><?php the_field('evenement_texte_2'); ?></p>
 
-
-    </div>
-    <div class="actions_recentes__evenement-3">
-        <?php if (get_field('evenement_image_3')): ?>
-            <img class="actions_recentes__evenement-img" src="<?php the_field('evenement_image_3'); ?>"/>
-        <?php endif; ?>
-
-        <h3 class="actions_recentes__evenement-title"><?php the_field('evenement_titre_3'); ?></h3>
-        <p class="actions_recentes__evenement-texte"><?php the_field('evenement_texte_3'); ?></p>
-    </div>
-</section>
-
-<section class="actions_recentes__agenda">
-    <div class="actions_recentes__agenda-1">
-        <h4 class="actions_recentes__agenda_date"><?php the_field('agenda_date'); ?></h4>
-        <p class="actions_recentes__agenda_texte"><?php the_field('agenda_texte'); ?></p>
-    </div>
-    <div class="actions_recentes__agenda-1">
-        <h4 class="actions_recentes__agenda_date"><?php the_field('agenda_date_2'); ?></h4>
-        <p class="actions_recentes__agenda_texte"><?php the_field('agenda_texte_2'); ?></p>
-    </div>
-    <div class="actions_recentes__agenda-2">
-        <h4 class="actions_recentes__agenda_date"><?php the_field('agenda_date_3'); ?></h4>
-        <p class="actions_recentes__agenda_texte"><?php the_field('agenda_texte_3'); ?></p>
-    </div>
-    <div class="actions_recentes__agenda-3">
-        <h4 class="actions_recentes__agenda_date"><?php the_field('agenda_date_4'); ?></h4>
-        <p class="actions_recentes__agenda_texte"><?php the_field('agenda_texte_4'); ?></p>
-    </div>
-    <div class="actions_recentes__agenda-4">
-        <h4 class="actions_recentes__agenda_date"><?php the_field('agenda_date_5'); ?></h4>
-        <p class="actions_recentes__agenda_texte"><?php the_field('agenda_texte_5'); ?></p>
-    </div>
-</section>
-<!--lien section-->
-<section class="actions_recentes-lien">
-    <div class="actions_recentes__lien-gauche">
-        <h4 class="actions_recentes__lien-titre"><?php the_field('lien_titre'); ?></h4>
-        <p class="actions_recentes__lien_description"><?php the_field('lien_description'); ?></p>
-        <a class="actions_recentes__hero-button"
-           href="<?php the_field('lien_bouton_url'); ?>"><?php the_field('lien_bouton'); ?></a>
-    </div>
-    <div class="actions_recentes__lien-droite">
-        <h4 class="actions_recentes__lien-titre"><?php the_field('lien_titre_2'); ?></h4>
-        <p class="actions_recentes__lien_description"><?php the_field('lien_description_2'); ?></p>
-        <a class="actions_recentes__hero-button"
-           href="<?php the_field('lien_bouton_url_2'); ?>"><?php the_field('lien_bouton_2'); ?></a>
-    </div>
-</section>
-<?php get_footer(); //appel du template header.php  ?>
+        <!--lien section-->
+        <article class="envies">
+            <h3 class="envies__title"><?php the_field('lien_titre'); ?></h3>
+            <p class="envies__txt"><?php the_field('lien_description'); ?></p>
+            <a class="envies__btn"
+               href="<?php the_field('lien_bouton_url'); ?>"><?php the_field('lien_bouton'); ?></a>
+        </article>
+        <article class="travel">
+            <h4 class="travel__title"><?php the_field('lien_titre_2'); ?></h4>
+            <p class="travel__txt"><?php the_field('lien_description_2'); ?></p>
+            <a class="travel__btn"
+               href="<?php the_field('lien_bouton_url_2'); ?>"><?php the_field('lien_bouton_2'); ?></a>
+        </article>
+</main>
+<?php get_footer(); //appel du template header.php
+?>
