d2fa9b7c3c056e153ccc6327731a566e7b0af340
armandfardeau <fardeauarmand@gmail.com>
2016-11-16 19:05:08 +0100

    add font awesome

--- wp-content/themes/irys/inc/scripts_styles.php
+++ wp-content/themes/irys/inc/scripts_styles.php
@@ -1,30 +1,34 @@
 <?php
 
-function ajout_scripts() {
-  
-  
+function ajout_scripts()
+{
+
+
 // enregistrement d'un nouveau script
-wp_register_script('main', get_template_directory_uri() . '/scripts/main.js', array('jquery'),'1.0', true);
+    wp_register_script('main', get_template_directory_uri() . '/scripts/main.js', array('jquery'), '1.0', true);
 
 // appel du script dans la page
-wp_enqueue_script('main');
-  
-wp_localize_script('main', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
-  
+    wp_enqueue_script('main');
 
-  // enregistrement d'un nouveau style
-wp_register_style( 'google-font', 'https://fonts.googleapis.com/css?family=PT+Sans+Narrow' );
+    wp_localize_script('main', 'ajaxurl', admin_url('admin-ajax.php'));
+
+    wp_register_style('font-awesome', 'https://use.fontawesome.com/d6bf6738a1.js');
+
+    wp_enqueue_style('font-awesome');
+
+    // enregistrement d'un nouveau style
+    wp_register_style('google-font', 'https://fonts.googleapis.com/css?family=PT+Sans+Narrow');
 
 // appel du style dans la page
-wp_enqueue_style( 'google-font' );
-  
-  
-  
+    wp_enqueue_style('google-font');
+
+
 // enregistrement d'un nouveau style
-wp_register_style( 'main_style', get_template_directory_uri() . '/styles/main.css' );
+    wp_register_style('main_style', get_template_directory_uri() . '/styles/main.css');
 
 // appel du style dans la page
-wp_enqueue_style( 'main_style' );
+    wp_enqueue_style('main_style');
 
 }
-add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
+
+add_action('wp_enqueue_scripts', 'ajout_scripts');
